044080fe82f394edacdb9cf429a8f90290a10368
Lucas Fitchett <lucasfitchett04@gmail.com>
2025-05-08 14:19:48 -0700

    first draft

---
+++ src/main.js
@@ -0,0 +1,25 @@
+const grid = document.getElementById("grid")
+let mouseDown = false;
+
+grid.addEventListener("mousedown", () => {
+	mouseDown = true
+})
+
+document.addEventListener("mouseup", () => {
+	mouseDown = false
+})
+
+for (let i = 0; i < 64 * 64; i++) {
+	const div = document.createElement("div")
+	div.classList.add("aspect-square", "bg-gray-100")
+	grid.appendChild(div)
+
+	div.addEventListener("mouseover", draw)
+	div.addEventListener("mousedown", draw)
+}
+
+function draw(event) {
+	if (mouseDown === true) {
+		event.target.style.backgroundColor = "MediumTurquoise";
+	}
+}
