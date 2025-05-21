74a4aa9c9fc599d7412c8ccaeec39c62364ea796
Lucas Fitchett <lucasfitchett04@gmail.com>
2025-05-20 17:00:59 -0700

    click event listener

--- src/main.js
+++ src/main.js
@@ -1,25 +1,28 @@
-const grid = document.getElementById("grid")
+const grid = document.getElementById("grid");
 let mouseDown = false;
 
 grid.addEventListener("mousedown", () => {
-	mouseDown = true
-})
+    mouseDown = true;
+});
 
 document.addEventListener("mouseup", () => {
-	mouseDown = false
-})
+    mouseDown = false;
+});
 
 for (let i = 0; i < 64 * 64; i++) {
-	const div = document.createElement("div")
-	div.classList.add("aspect-square", "bg-gray-100")
-	grid.appendChild(div)
+    const div = document.createElement("div");
+    div.classList.add("aspect-square", "bg-gray-100");
+    grid.appendChild(div);
 
-	div.addEventListener("mouseover", draw)
-	div.addEventListener("mousedown", draw)
+    div.addEventListener("mouseover", draw);
+    div.addEventListener("mousedown", draw);
+    div.addEventListener("click", (event) => {
+        event.target.style.backgroundColor = "MediumTurquoise";
+    });
 }
 
 function draw(event) {
-	if (mouseDown === true) {
-		event.target.style.backgroundColor = "MediumTurquoise";
-	}
+    if (mouseDown === true) {
+        event.target.style.backgroundColor = "MediumTurquoise";
+    }
 }
